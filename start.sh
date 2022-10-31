3adff7ce4f7bd4947d0c68798a59de1ae2b75338
TGDarkLord <113157573+TGDarkLord@users.noreply.github.com>
2022-10-31 12:53:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/JOSProjects/IMDb-Movie-Bot.git /IMDb-Movie-Bot
-cd /IMDb-Movie-Bot
+git clone -b master https://github.com/TGDarkLord/UniqueMovie-Bot.git /UniqueMovie-Bot
+cd /UniqueMovie-Bot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
+echo "Starting Bot...🤖"
 python3 bot.py
