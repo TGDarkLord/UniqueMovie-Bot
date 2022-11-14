d86bbed5b4776dd358e9fd5b05ecbeca1c5d4450
TGDarkLord <113157573+TGDarkLord@users.noreply.github.com>
2022-11-14 19:10:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,7 +11,7 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-SESSION_NAME = environ.get('SESSION_NAME', 'Media_search')
+SESSION = environ.get('SESSION', 'Media_Search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
